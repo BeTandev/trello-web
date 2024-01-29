1837b4c012d0a5f40d8f1d1294bca15ee6fcceac
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-01-29 23:17:00 +0700

    setup theme MUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Button from '@mui/material/Button'
 import AssistantIcon from '@mui/icons-material/Assistant'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>Be Tan</div>
+
+      <Typography variant="body2" color="text.secondary">test Typo</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
